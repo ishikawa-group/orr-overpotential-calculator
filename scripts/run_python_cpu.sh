34aeb05685dbad85cabdb9f323db08ff13b7a568
Wakamiya-tsubame <wakamiya.t.b887@m.isct.ac.jp>
2025-05-19 17:49:43 +0900

    Initial commit

--- scripts/run_python_cpu.sh
+++ scripts/run_python_cpu.sh
@@ -3,8 +3,8 @@
 #$ -l cpu_160=1
 #$ -l h_rt=24:00:00
 #$ -N RPBE_ORR
-#$ -o /gs/fs/tga-ishikawalab/wakamiya/Pt111_RPBE/result/log/RPBE_output.log
-#$ -e /gs/fs/tga-ishikawalab/wakamiya/Pt111_RPBE/result/log/RPBE_error.log
+#$ -o /gs/fs/tga-ishikawalab/wakamiya/orr_overpotential_calculator/result/log/RPBE_output.log
+#$ -e /gs/fs/tga-ishikawalab/wakamiya/orr_overpotential_calculator/result/log/RPBE_error.log
 
 # Load required modules
 module load intel
@@ -16,7 +16,7 @@ source /gs/fs/tga-ishikawalab/wakamiya/python_virtual_env/ORR_catalyst_generator
 
 # Set VASP pseudopotential path for ASE
 export VASP_PP_PATH=/gs/fs/tga-ishikawalab/vasp/potential
-export VASP_SCRIPT=/gs/fs/tga-ishikawalab/wakamiya/Pt111_RPBE/run_vasp/run_vasp.py
+export VASP_SCRIPT=/gs/fs/tga-ishikawalab/wakamiya/orr_overpotential_calculator/src/orr_overpotential_calculator/run_vasp/run_vasp.py
 
 # Run python script
-python3 /gs/fs/tga-ishikawalab/wakamiya/Pt111_RPBE/run_test.py
+python3 /gs/fs/tga-ishikawalab/wakamiya/orr_overpotential_calculator/scripts/run_test.py
