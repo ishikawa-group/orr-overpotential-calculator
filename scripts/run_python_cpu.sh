91b8a5417b90d28ed1905042bb540f6b15956e0f
Wakamiya0315 <jagimi11301360@gmail.com>
2025-12-09 13:17:56 +0900

    Use sample paths in run_python_cpu.sh

--- scripts/run_python_cpu.sh
+++ scripts/run_python_cpu.sh
@@ -2,9 +2,9 @@
 #$ -cwd
 #$ -l cpu_160=1
 #$ -l h_rt=24:00:00
-#$ -N ORR_Ir111
-#$ -o /example/Ir111/log/RPBE_output.log
-#$ -e /example/Ir111/log/RPBE_error.log
+#$ -N ORR_Ir111_sample
+#$ -o ./example/Ir111/log/RPBE_output.log
+#$ -e ./example/Ir111/log/RPBE_error.log
 
 # Load required modules
 module load intel
@@ -12,11 +12,11 @@ module load intel-mpi
 module load cuda
 
 # Activate Python virtual environment
-source /gs/fs/tga-ishikawalab/wakamiya/python_virtual_env/ORR_catalyst_generator_env/bin/activate
+source /path/to/venv/bin/activate
 
 # Set VASP pseudopotential path for ASE
-export VASP_PP_PATH=/gs/fs/tga-ishikawalab/vasp/potential
-export VASP_SCRIPT=/src/orr_overpotential_calculator/run_vasp/run_vasp.py
+export VASP_PP_PATH=/path/to/vasp/pseudopotentials
+export VASP_SCRIPT=/path/to/repo/src/orr_overpotential_calculator/run_vasp/run_vasp.py
 
 # Run python script
-python3 /example/run_test.py
+python3 ./example/run_test.py
